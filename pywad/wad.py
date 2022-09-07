70aca7422d38e369128ebb23fd62486ac49e4387
Alex Rembish <rembish@shoptet.cz>
2022-09-07 15:44:16 +0200

    Reading directory entries

--- pywad/wad.py
+++ pywad/wad.py
@@ -1,7 +1,23 @@
+from functools import cached_property
+from io import SEEK_SET
 from struct import unpack, calcsize
 
 from .enums import WadType
 from .exceptions import BadHeaderWadException
+
+
+class DirectoryEntry:
+    def __init__(self, owner, offset, size, name):
+        self.owner = owner
+        self.name = name.decode("ascii")
+        self.size = size
+        self.offset = offset
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} "{self.name}" / {self.size}>'
 
 
 class WadFile:
@@ -9,7 +25,7 @@ class WadFile:
         self.fd = open(filename, "rb")
 
         header_format = "<4sII"
-        magic, self.number_of_lumps, self.directory_offset = \
+        magic, self.directory_size, self._directory_offset = \
             unpack(header_format, self.fd.read(calcsize(header_format)))
         magic = magic.decode("ascii")
         if magic not in ("IWAD", "PWAD"):
@@ -26,3 +42,14 @@ class WadFile:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
+
+    @cached_property
+    def directory(self):
+        lump_format = "<II8s"
+        self.fd.seek(self._directory_offset, SEEK_SET)
+
+        entries = []
+        for i in range(self.directory_size):
+            lump = unpack(lump_format, self.fd.read(calcsize(lump_format)))
+            entries.append(DirectoryEntry(self, *lump))
+        return entries
